8e54b751bbe1d324dea0ce5b2470c3d2a389ba51
Ejikeomeke <engromeke@gmail.com>
2025-05-10 09:28:28 +0100

    loading function added

--- etl.py
+++ etl.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import requests
 import json
-import yaml
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
-from datetime import timedelta, datetime
+import os
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from airflow.operators.python import PythonOperator
 
-# function to fetch the api keys
-def connection():
-    with open('key.yaml', 'r') as file:
-        config = yaml.safe_load(file)
 
-        key = config.get('key')
-    return key
+# to load the python environmnet variables
+load_dotenv()
 
 #function to extract data using the latitude and longitude of the city Abuja
 def get_data(ti):
   lati = 9.072264
   longi = 7.491302
-  API_key = connection()
+  API_key = os.getenv("WEATHER_API") # passing the api keys from env file
 
   source = f"https://api.openweathermap.org/data/2.5/weather?lat={lati}&lon={longi}&appid={API_key}"
   response = requests.get(url=source)
@@ -32,21 +29,20 @@ def get_data(ti):
 # data transformation function
 def transformation(ti):
   weather_data = []
-  raw_data = ti.xcom_pull(key='extracted_data', task_ids='get_data') # pulling data from the extraction task
+  raw_data = ti.xcom_pull(key='extracted_data', task_ids='get_data')
   country = raw_data['sys']['country']
   latitude = raw_data['coord']['lat']
   longitude = raw_data['coord']['lon']
   city_name = raw_data['name']
   time_zone = raw_data['timezone']
 
-  # transforming datetime to a datetime object
+# transforming datetime to a datetime object
   date = datetime.fromtimestamp(raw_data['dt']).date().strftime("%Y-%m-%d %H:%M:%S")
   time_of_calculation = datetime.fromtimestamp(raw_data['dt']).time().strftime("%Y-%m-%d %H:%M:%S")
   sunrise_time = datetime.fromtimestamp(raw_data['sys']['sunrise']).time().strftime("%Y-%m-%d %H:%M:%S")
   sunset_time = datetime.fromtimestamp(raw_data['sys']['sunset']).time().strftime("%Y-%m-%d %H:%M:%S")
 
-
-  cloud = raw_data['clouds']
+  cloud = raw_data['clouds'] 
   #transforming temperature from degree kelvin to degree celcuis 
   temperature_in_degree = raw_data['main']['temp']-273.15
   min_temp_in_degree =  raw_data['main']['temp_min']-273.15
@@ -56,7 +52,7 @@ def transformation(ti):
   wind_speed = raw_data['wind']['speed']
   wind_degree = raw_data['wind']['deg']
   wind_gust = raw_data['wind']['gust']
-  
+
   weather_data.append({
       "country":country,
       "latitude":latitude,
@@ -76,8 +72,30 @@ def transformation(ti):
       "wind_gust":wind_gust
 
   })
-  print(weather_data)
-  return weather_data
+  transformed_data = weather_data
+  ti.xcom_push(key='transformed_data', value=transformed_data)
+  return transformed_data
+
+
+def load_data(ti):
+  weather_data = ti.xcom_pull(key='transformed_data', task_ids='data_transformation')
+  df = pd.Series(weather_data)
+  connection_str = os.getenv("CONTAINER_STRING")
+  container_name = os.getenv("CONTAINER_NAME")
+    
+    # create a blobserviceClient
+  blob_service_client = BlobServiceClient.from_connection_string(connection_str)
+  container_client = blob_service_client.get_container_client(container_name)
+
+  files = [(df, 'raw/transformed_weather_data.csv')]
+
+  for file, blob_name in files:
+    blob_client = container_client.get_blob_client(blob_name)
+    output = file.to_csv(index=False)
+    blob_client.upload_blob(output, overwrite=True)
+    print(f"{blob_name} loaded into Azure succesfully")
+    
+
 
 # airflow default arguements
 default_args = {
@@ -93,10 +111,11 @@ default_args = {
 
 #airflow dag
 dag = DAG(
-    'energy_etl',
+    'weather_energy_etl',
     default_args =default_args,
-    description='energy weather etl data pipeline'
+    description='weather etl data pipeline'
 )
+
 
 # extraction task
 extraction = PythonOperator(
@@ -105,6 +124,7 @@ extraction = PythonOperator(
     dag=dag,
 )
 
+
 #transformation task
 transformation = PythonOperator(
     task_id='data_transformation',
@@ -112,4 +132,12 @@ transformation = PythonOperator(
     dag=dag,
 )
 
-extraction >> transformation
+#loading data to Azure Storage Blob
+loading = PythonOperator(
+    task_id='load_data',
+    python_callable=load_data,
+    dag=dag,
+)
+
+
+extraction >> transformation >> loading
